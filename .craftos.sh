2457ee4937edd19c6707e15fa73bb54cf09fa4bb
Sebastian Hens <hens@auxmoney.com>
2021-02-18 08:28:17 +0100

    feat(): Added linux starter and moved env to dist variation

--- .craftos.sh
+++ .craftos.sh
@@ -13,7 +13,5 @@ if [[ "$OSTYPE" == "win32"* ]] || [[ "$OSTYPE" == "msys"* ]]
 then
     start "" "$CRAFT_OS" --mount-rw "lystrain"="$MOUNT"
 else
-    echo "LINUX COMMAND HERE"
+    craftos --mount-rw "lystrain"="$MOUNT"
 fi
-
-# alias lystrain="start \"\" /c/Program\ Files/CraftOS-PC/CraftOS-PC.exe --mount-rw \"lystrain\"=\"E:\workspaces\applications\minecraft-cc\lystrain\"; code /e/workspaces/applications/minecraft-cc/lystrain"
